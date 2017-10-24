74e8adf9c171916797db4d2a16a487b7282e79c0
chenhe82166 <32590367+chenhe82166@users.noreply.github.com>
2017-10-24 11:24:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,17 +8,18 @@ import numpy as np
 batch_size = 1
 
 ckpt = tf.train.get_checkpoint_state("save")
-saver = tf.train.import_meta_graph(ckpt.model_checkpoint_path + '.meta')
-
-pred = tf.get_collection("pred")[0]
+saver = tf.train.import_meta_graph(ckpt.model_checkpoint_path + '.meta')# 恢复tensorflow图，也就是读取神经网络的结构，从而无需再次构建网络
+pred = tf.get_collection("pred")[0]# tf.get_collection：从一个结合中取出全部变量，是一个列表
 x = tf.get_collection("x")[0]
 keep_prob = tf.get_collection("keep_prob")[0]
 
 # Launch the graph
 # with tf.Session() as sess:
 sess = tf.Session()
-saver.restore(sess, ckpt.model_checkpoint_path)
-
+saver.restore(sess, ckpt.model_checkpoint_path)# 测试阶段使用saver.restore()方法恢复变量：
+                                               # sess：表示当前会话，之前保存的结果将被加载入这个会话
+                                               # ckpt.model_checkpoint_path：表示模型存储的位置，不需要提供模型的名字，它会去查看checkpoint文件，看看最新的是谁，叫做什么。
+  
 # test
 step_test = 1
 while step_test * batch_size < len(testing):
